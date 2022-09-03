7589135d028dc0af51b526e277a3893aee2f7841
prakash <prakash@prakash.com>
2022-09-03 14:38:20 +1000

    Add api operations

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# simple-flask-api
-Simple Flask API for playing with docker and kubernetes
+# Simple Flask API
+
+Simple Flask based API for playing with Docker and Kubernetes.
+
+## Run the app from local machine
+
+* Execute the following commands:
+```
+cd src
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+flask run
+```
+* Browse http://localhost:5000
+
+## Run the app from Docker container
+
+* Build docker image and start the container
+```
+docker build -t flaskapp:1 .
+docker run -p 8080:8080 -d flaskapp:1
+```
+* Browse http://localhost:8080
+
+
---
+++ src/app.py
@@ -0,0 +1,45 @@
+
+from urllib import request
+from flask import Flask, render_template, jsonify, request
+
+
+app = Flask(__name__)
+
+#------------------------------------------------------------
+
+
+@app.route('/', methods=['GET'])
+def index():
+    resp = {'name': 'HelloWorld'}
+    return jsonify(resp)
+
+#------------------------------------------------------------
+
+
+@app.route('/add', methods=['GET'])
+def add():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+
+    resp = {'sum': a+b}
+    return jsonify(resp)
+
+#------------------------------------------------------------
+
+@app.route('/customer/<id>', methods=['GET'])
+def customer(id):
+
+    resp = {
+        'customerId': id,
+        'firstName': 'Jack',
+        'lastName': 'Sparrow',
+        'city': 'Melbourne',
+        'interests': ['Swimming', 'Adventure', 'Travel']
+    }
+    return jsonify(resp)
+
+#------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
